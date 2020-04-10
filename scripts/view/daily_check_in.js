1a5c5bcb78e6582d1fff4a22454d4cbd4d1865c8
zhihaofans <zhihaofans@hotmail.com>
2020-04-11 01:23:54 +0800

    Update

---
+++ scripts/view/daily_check_in.js
@@ -0,0 +1,11 @@
+let acfun = require("../api/acfun.js"),
+    bilibili = require("../api/bilibili.js");
+let initView = () =>{
+  
+};
+let acfun = () =>{
+  acfun.signIn();
+};
+let bilibiliComic = () =>{
+  bilibili.mangaClockin();
+};
