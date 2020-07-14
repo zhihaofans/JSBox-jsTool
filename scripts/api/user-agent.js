8fe9d66f18dd0f521e6615c5a09dbc71e03c5a79
zhihaofans <zhihaofans@hotmail.com>
2020-07-14 10:34:40 +0800

    新增Bilibili概念版(ios 14)的UA

--- scripts/api/user-agent.js
+++ scripts/api/user-agent.js
@@ -3,8 +3,9 @@ let ACFUN = {
 };
 let BILIBILI = {
     APP_IPHONE: "bili-universal/9320 CFNetwork/1125.2 Darwin/19.5.0 os/ios model/iPhone 11 mobi_app/iphone osVer/13.4.5 network/1",
-    VIP_CHECKIN: "bili-universal/9290 CFNetwork/1125.2 Darwin/19.4.0",
-    COMIC: "comic-universal/802 CFNetwork/1125.2 Darwin/19.4.0 os/ios model/iPhone 11 mobi_app/iphone_comic osVer/13.4 network/2"
+    BLUE_IOS: "bili-blue/10150 CFNetwork/1183 Darwin/20.0.0 os/ios model/iPhone12,8 mobi_app/iphone_b build/10150 osVer/14.0 network/1 channel/AppStore",
+    COMIC: "comic-universal/802 CFNetwork/1125.2 Darwin/19.4.0 os/ios model/iPhone 11 mobi_app/iphone_comic osVer/13.4 network/2",
+    VIP_CHECKIN: "bili-universal/9290 CFNetwork/1125.2 Darwin/19.4.0"
 };
 let KAAASS = "JSBox-jsTool/0.1 (github:zhuangzhihao-io) <zhuang@zhihao.io>"; // 请尊重API提供者
 module.exports = {
