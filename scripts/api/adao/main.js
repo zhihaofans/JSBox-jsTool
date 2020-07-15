84c0acb9c9f2d73ec7b819a2a9a9c5bf27121df9
zhihaofans <zhihaofans@hotmail.com>
2020-07-15 17:42:02 +0800

    Update

--- scripts/api/adao/main.js
+++ scripts/api/adao/main.js
@@ -1,23 +1,11 @@
 let API_URL = require("./api_url.js");
 
 function getBackupUrl() {
-    return $http.get({
-        url: API_URL.GET_BACKUP_URL
-    });
-    /* .then(function (resp) {
-            var data = resp.data;
-        }) */
-
+  return await $http.get({ url: API_URL.GET_BACKUP_URL }).data;
 }
 
 function getTimeLine() {
-    return $http.get({
-        url: API_URL.GET_TIMELINE
-    });
-    /* .then(function (resp) {
-            var data = resp.data;
-        }) */
-
+  return await $http.get({ url: API_URL.GET_TIMELINE }).data;
 }
 
 module.exports = {
