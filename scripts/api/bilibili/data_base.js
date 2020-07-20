025b9cbbacbca85303e512388eac6a2a3dc363af
zhihaofans <zhihaofans@hotmail.com>
2020-07-20 11:57:45 +0800

    Update scripts/api/bilibili/data_base.js

--- scripts/api/bilibili/data_base.js
+++ scripts/api/bilibili/data_base.js
@@ -1,6 +1,21 @@
-function load(){
-  $cache.getAsync()
+function open(db_name) {
+  return $sqlite.open(`${db_name}.db`);
+}
+
+function close(db) {
+  return $sqlite.close(db);
+}
+
+function load(db_name, table_name, key_id) {
+  const db = open(`${db_name}.db`);
+  db.query({
+    sql: "SELECT * FROM ? where id = ?",
+    args: [table_name, key_id]
+  }, (rs, err) => {
+
+  });
+  close(db);
 }
 module.exports = {
-  
+  open
 };
