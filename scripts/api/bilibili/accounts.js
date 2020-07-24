8d40b5737f82e34afa94a91b220111049e8fd06b
zhihaofans <zhihaofans@hotmail.com>
2020-07-24 08:28:41 +0800

    Update scripts/api/bilibili/accounts.js

--- scripts/api/bilibili/accounts.js
+++ scripts/api/bilibili/accounts.js
@@ -1,37 +1,121 @@
-function getAccesskeyList(uid) {
-    const cacheId = "BILIBILI_ACCOUNTS_ACCESS_KEY";
-    const aak = $cache.get(cacheId);
-    if (aak) {
-        const aakl = JSON.parse(aak);
-        if (aakl) {
-            if (aakl[uid]) {
-                return aakl[uid];
+let access_key_list_cacheId = `BILIBILI_ACCOUNTS_ACCESS_KEY`;
+let sys = require("../system.js");
+
+function getAccessKey(uid) {
+    if (uid) {
+        var accessKeyList = getAccessKeyList();
+        if (accessKeyList) {
+            if (uid in accessKeyList) {
+                return accessKeyList[uid];
             } else {
-                setAccesskeyList(uid, []);
-                return [];
+                $console.error(`getAccesskey:${uid}不存在`);
             }
         } else {
-            return null;
+            $console.error(`access_key_list:null`);
         }
     } else {
-        setAccesskeyList(uid, []);
-        return [];
+        $console.error(`uid:null`);
     }
 }
 
-function setAccesskeyList(uid, access_key_list) {
-    const cacheId = "BILIBILI_ACCOUNTS_ACCESS_KEY";
-    var aak = $cache.get(cacheId);
-    aak[uid] = access_key_list;
-    $cache.set(cacheId, aak);
-}
-
-function addAccesskey(uid, access_key) {
-    var old_list = getAccesskeyList(uid);
-    if (old_list == null) {
-        old_list = [access_key];
+function setAccessKey(uid, access_key) {
+    if (uid && access_key) {
+        var accessKeyList = getAccessKeyList();
+        if (accessKeyList) {
+            $console.info(accessKeyList);
+            accessKeyList[uid] = access_key;
+            $console.info(accessKeyList);
+            setAccessKeyList(accessKeyList);
+        } else {
+            setAccessKeyList({
+                uid: access_key
+            });
+        }
     } else {
-        old_list.push(access_key);
+        $console.error(`uid && access_key:null`);
     }
-    setAccesskeyList(old_list);
 }
+
+function setAccessKeyList(access_key_list) {
+    $console.info(access_key_list);
+    $cache.set(access_key_list_cacheId, JSON.stringify(access_key_list));
+}
+
+function getAccessKeyList() {
+    const accessKeyList = $cache.get(access_key_list_cacheId);
+    $console.info(accessKeyList);
+    return accessKeyList ? JSON.parse(accessKeyList) : null;
+}
+
+function remoteAccessKey(uid) {
+    var accessKeyList = getAccessKeyList();
+    if (accessKeyList) {
+        if (uid in accessKeyList) {
+            $console.info(accessKeyList);
+            delete accessKeyList[uid];
+            $console.info(accessKeyList);
+            setAccessKeyList(accessKeyList);
+        } else {
+            $console.error(`access_key_list:${uid}不存在`);
+        }
+    } else {
+        $console.error(`access_key_list:null`);
+    }
+
+}
+
+function remoteAccessKeyList() {
+    $cache.remove(access_key_list_cacheId);
+}
+
+function backupAccessKeyList(cloud = false) {
+    const accessKeyList = getAccessKeyList();
+
+    if (accessKeyList) {
+        $console.info(accessKeyList);
+        const accessKeyJson = JSON.stringify(accessKeyList);
+        if (cloud) {
+            $drive.save({
+                data: $data({
+                    string: accessKeyJson
+                }),
+                name: `bilibili_accesskey_list_backup_${sys.getNowUnixTime()}.json`,
+                handler: function () {
+                    $ui.alert({
+                        title: "保存完毕",
+                        message: "请确认是否保存成功",
+                    });
+                }
+            })
+        } else {
+            const saveDir = "/.backup/bilibili/";
+            var success = $file.mkdir(saveDir);
+            if (success) {
+                var success = $file.write({
+                    data: $data({
+                        string: accessKeyJson
+                    }),
+                    path: `${saveDir}bilibili_accesskey_list_backup_${sys.getNowUnixTime()}.json`
+                });
+                $ui.alert({
+                    title: "保存完毕",
+                    message: success ? "成功" : "失败",
+                });
+            } else {
+                $console.error(`backup:创建文件夹失败`);
+            }
+        }
+    } else {
+        $console.error(`access_key_list:null`);
+    }
+}
+
+module.exports = {
+    backupAccessKeyList,
+    getAccessKey,
+    getAccessKeyList,
+    setAccessKey,
+    setAccessKeyList,
+    remoteAccessKey,
+    remoteAccessKeyList
+};
