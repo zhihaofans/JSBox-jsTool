e6ff9cce06b80a3615ae473fb49c3f473ae7fc6a
zhihaofans <zhihaofans@hotmail.com>
2020-08-26 14:48:18 +0800

    首页ui代码移至/scripts/view/

--- main.js
+++ main.js
@@ -1,36 +1,5 @@
-let page = require("./scripts/page_init.js");
-let siteListL10n = [
-    "ACFUN",
-    "BILIBILI",
-    "INSTAGRAM",
-    "每日签到",
-    "Bilichat历史"
-];
-let moreListL10n = ["CDN", "IMAGE", "杂项"];
-let getNavButton = () => {
-    return [
-        {
-            title: $l10n("MENU"),
-            icon: "067", // Or you can use icon name
-            handler: () => {
-                $ui.menu({
-                    items: ["SCAN_QRCODE", "TEST"].map(x => $l10n(x)),
-                    handler: function(title, idx) {
-                        switch (idx) {
-                            case 0:
-                                page.scanQrcodeToGo();
-                                break;
-                            case 1:
-                                page.test();
-
-                                break;
-                        }
-                    }
-                });
-            }
-        }
-    ];
-};
+let page = require("/scripts/page_init"),
+    main = require("/scripts/view/main");
 
 function checkCacheDir() {
     const cacheDir = ".cache/";
@@ -49,7 +18,6 @@ function checkCacheDir() {
 }
 
 function init() {
-    // app.faceId();
     var query = $context.query;
     if (query.mod) {
         page.contextOpen(query);
@@ -59,86 +27,7 @@ function init() {
         if (!checkCacheDir()) {
             $console.error("初始化缓存目录失败");
         }
-        $ui.render({
-            props: {
-                id: "main",
-                homeIndicatorHidden: false,
-                modalPresentationStyle: 0,
-                navButtons: getNavButton()
-            },
-            views: [
-                {
-                    type: "list",
-                    props: {
-                        data: [
-                            {
-                                title: $l10n("SITE"),
-                                rows: siteListL10n.map(x => $l10n(x))
-                            },
-                            {
-                                title: $l10n("更多"),
-                                rows: moreListL10n.map(x => $l10n(x))
-                            }
-                        ]
-                    },
-                    layout: $layout.fill,
-                    events: {
-                        didSelect: function(_sender, indexPath, _data) {
-                            switch (indexPath.section) {
-                                case 0:
-                                    switch (indexPath.row) {
-                                        case 0:
-                                            page.acfun();
-                                            break;
-                                        case 1:
-                                            page.bilibili();
-                                            break;
-                                        case 2:
-                                            page.instagram();
-                                            break;
-                                        case 3:
-                                            page.dailyCheckin();
-                                            break;                                 
-                                        case 4:
-                                            page.bilichat();
-                                            break;
-                                        default:
-                                            $ui.error("错误选项");
-                                    }
-                                    break;
-                                case 1:
-                                    switch (indexPath.row) {
-                                        case 0:
-                                            page.cdn();
-                                            break;
-                                        case 1:
-                                            page.image();
-                                            break;
-                                        case 2:
-                                            page.misc();
-                                            break;
-                                        default:
-                                            $ui.error("错误选项");
-                                    }
-                                    break;
-                                default:
-                                    $ui.error("错误选项");
-                            }
-                        }
-                    }
-                }
-            ],
-            events: {
-                appeared: function() {
-                    $app.tips(
-                        "右上角的按钮是更新按钮，摇一摇设备也可以触发检测更新"
-                    );
-                },
-                shakeDetected: function() {
-                    //摇一摇
-                }
-            }
-        });
+        main.loadMainView();
     }
 }
 
