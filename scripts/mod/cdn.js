af854b330bfc9935aa848fd7c10bd66d2dc9c92e
zhihaofans <zhihaofans@hotmail.com>
2020-08-30 21:38:14 +0800

    Update

---
+++ scripts/mod/cdn.js
@@ -0,0 +1,30 @@
+function initView() {
+    $ui.push({
+        props: {
+            title: "CDN"
+        },
+        views: [
+            {
+                type: "list",
+                props: {
+                    data: [
+                        {
+                            title: "Github",
+                            rows: ["0-0"]
+                        }
+                    ]
+                },
+                layout: $layout.fill,
+                events: {
+                    didSelect: function(_sender, indexPath, _data) {
+                        const section = indexPath.section;
+                        const row = indexPath.row;
+                    }
+                }
+            }
+        ]
+    });
+}
+module.exports = {
+    init: initView
+};
--- scripts/mod/news.js
+++ scripts/mod/news.js
@@ -6,6 +6,27 @@ function init() {
     if (newsList) {
         if (newsList.length > 0) {
             $console.info(newsList);
+            $ui.push({
+                props: {
+                    title: "新闻模块"
+                },
+                views: [
+                    {
+                        type: "list",
+                        props: {
+                            data: newsList
+                        },
+                        layout: $layout.fill,
+                        events: {
+                            didSelect: function(_sender, indexPath, _data) {
+                                const section = indexPath.section;
+                                const row = indexPath.row;
+                                initMod(newsList[row]);
+                            }
+                        }
+                    }
+                ]
+            });
         } else {
             $ui.error("0个新闻模组");
         }
@@ -23,7 +44,38 @@ function init() {
         });
     }
 }
-
+function initMod(modName) {
+    const fileName = `${newsDir}${modName}`;
+    if ($file.exists(fileName)) {
+        if ($file.isDirectory(fileName)) {
+            $ui.error("这是目录");
+        } else {
+            const modData = require(fileName);
+            if (modData) {
+                try {
+                    modData.init();
+                    $console.info(`Mod加载完毕:${modName}`);
+                } catch (error) {
+                    $ui.alert({
+                        title: `${modName}加载失败`,
+                        message: error.message,
+                        actions: [
+                            {
+                                title: "OK",
+                                disabled: false, // Optional
+                                handler: function() {}
+                            }
+                        ]
+                    });
+                }
+            } else {
+                $ui.error("请确认是否为mod文件");
+            }
+        }
+    } else {
+        $ui.error("不存在该文件");
+    }
+}
 module.exports = {
     init
 };
