73aa961ab5bf110027833e854d1cdfc3f62b9422
zhihaofans <zhihaofans@hotmail.com>
2020-09-08 09:16:52 +0800

    Update app_scheme.js

--- scripts/api/app_scheme.js
+++ scripts/api/app_scheme.js
@@ -1,29 +1,33 @@
-let _URL = require("./urlData.js");
+//Alook浏览器
 let alookBrowserOpen = url => {
     $app.openURL(`Alook://${$text.URLEncode(url)}`);
 };
 let alookBrowserDownload = url => {
     $app.openURL(`Alook://download/${$text.URLEncode(url)}`);
 };
+// Chrome (使用jsbox接口)
 let chromeBrowserOpen = url => {
     $app.openBrowser({
         type: 10000,
         url: url
     });
 };
+// QQ浏览器 (使用jsbox接口)
 let qqBrowserOpen = url => {
     $app.openBrowser({
         type: 10003,
         url: url
     });
 };
+// 火狐浏览器 (使用jsbox接口)
 let firefoxBrowserOpen = url => {
     $app.openBrowser({
         type: 10002,
         url: url
     });
 };
-let safariPreview = (url) => {
+// Safari
+let safariPreview = url => {
     $safari.open({
         url: url
     });
@@ -38,33 +42,53 @@ let safariReadMode = (url, handler) => {
 let safariAddReadingItem = (url, title, previewText) => {
     $safari.addReadingItem({
         url: url,
-        title: title, // Optional
-        preview: previewText // Optional
+        title: title,
+        preview: previewText
     });
 };
+let safariOpen = (url, entersReader = false, height = 360, handler) => {
+    $safari.open({
+        url: url,
+        entersReader: entersReader,
+        height: height,
+        handler: handler
+    });
+};
+// Avplayer 视频播放器
 let avplayerVideo = url => {
     $app.openURL(`AVPlayer://${$text.URLEncode(url)}`);
 };
+// nPlayer 视频播放器
 let nplayerVideo = url => {
     $app.openURL(`nplayer-${url}`);
 };
+// Documents 文件管理器
 let documentsOpen = url => {
     $app.openURL(`r${url}`);
 };
+// Bilibili
 let bilibiliVideo = vid => {
     $app.openURL(getBilibiliVideoUrl(vid));
 };
 let getBilibiliVideoUrl = vid => {
-    return _URL.BILIBILI.BILIBILI_VIDEO + vid;
+    return `bilibili://video/${vid}`;
 };
+// Acfun
 let acfunVideo = vid => {
     $app.openURL(getAcfunVideoUrl(vid));
 };
 let getAcfunVideoUrl = vid => {
-    return _URL.ACFUN.ACFUN_DETAIL_VIDEO + vid;
+    return `acfun://detail/video/${vid}`;
 };
 let getAcfunVideoWebUrl = vid => {
-    return _URL.ACFUN.ACFUN_WWW_V_AC + vid;
+    return `https://www.acfun.cn/v/ac${vid}`;
+};
+// PPHub
+let pphubOpenUser = user => {
+    $app.openURL(`pphub://user?login=${user}`);
+};
+let pphubOpenRepository = (user, repository) => {
+    $app.openURL(`pphub://repo?owner=${user}&repo=${repository}`);
 };
 module.exports = {
     alookBrowserOpen,
@@ -83,4 +107,7 @@ module.exports = {
     acfunVideo,
     getAcfunVideoUrl,
     getAcfunVideoWebUrl,
+    safariOpen,
+    pphubOpenUser,
+    pphubOpenRepository
 };
