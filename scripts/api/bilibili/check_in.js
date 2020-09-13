3fe6fa26097c3573130aba63ff9520402c61de68
zhihaofans <zhihaofans@hotmail.com>
2020-09-13 10:55:58 +0800

    Update bilibili/check_in.js

--- scripts/api/bilibili/check_in.js
+++ scripts/api/bilibili/check_in.js
@@ -61,91 +61,49 @@ let mangaCheckin = async () => {
         });
     }
 };
-function mangaClockin() {
-    const accessKey = _USER.getAccessKey();
-    const uid = _USER.getUid();
-    if (accessKey && uid) {
-        $ui.loading(true);
-        $http.post({
-            url: _BILIURL.MANGA_CLOCK_IN,
-            header: {
-                "Content-Type": "application/x-www-form-urlencoded",
-                "User-Agent": _UA.BILIBILI.COMIC
-            },
-            body: {
-                platform: "ios",
-                uid: uid,
-                access_key: accessKey
-            },
-            handler: function(postResp) {
-                var clockinData = postResp.data;
-                $console.info(clockinData);
-                $ui.loading(false);
-                if (clockinData) {
-                    if (clockinData.code == 0) {
-                        $ui.alert({
-                            title: "签到结果",
-                            message: "签到成功"
-                        });
-                    } else {
-                        $ui.alert({
-                            title: `错误：${clockinData.code}`,
-                            message: clockinData.msg
-                        });
-                    }
-                } else {
-                    $ui.alert({
-                        title: "签到失败",
-                        message: "服务器返回空白结果"
-                    });
-                }
-            }
-        });
-    } else {
-        $ui.alert({
-            title: "签到失败",
-            message: "用户id或access_key为空，请登录"
-        });
-    }
-}
 
-function silverToCoin() {
+let silverToCoin = async () => {
     $ui.loading(true);
-    $http.post({
-        url: _BILIURL.SILVER_TO_COIN,
-        header: {
+    const postHeader = {
             "User-Agent": _UA.BILIBILI.APP_IPHONE,
             "Content-Type": "application/x-www-form-urlencoded"
         },
-        body: {
+        postBody = {
             access_key: _USER.getAccessKey()
-        },
-        handler: function(resp) {
-            var data = resp.data;
-            $ui.loading(false);
-            $console.info(data);
-            if (data) {
-                if (data.code == 0) {
-                    let silver2coinData = data.data;
-                    $ui.alert({
-                        title: data.message || data.msg || "兑换成功",
-                        message: `剩余银瓜子：${silver2coinData.silver}\n得到硬币：${silver2coinData.coin}`
-                    });
-                } else {
-                    $ui.alert({
-                        title: `错误${data.code}`,
-                        message: data.message || data.msg || "未知错误"
-                    });
-                }
+        };
+    const httpPost = await _HTTP.postAwait(
+        _BILIURL.SILVER_TO_COIN,
+        postBody,
+        postHeader
+    );
+    if (httpPost.error) {
+        $ui.loading(false);
+        $console.error(httpPost.error);
+    } else {
+        var data = httpPost.data;
+        $ui.loading(false);
+        $console.info(data);
+        if (data) {
+            if (data.code == 0) {
+                let silver2coinData = data.data;
+                $ui.alert({
+                    title: data.message || data.msg || "兑换成功",
+                    message: `剩余银瓜子：${silver2coinData.silver}\n得到硬币：${silver2coinData.coin}`
+                });
             } else {
                 $ui.alert({
-                    title: "错误",
-                    message: "空白数据"
+                    title: `错误${data.code}`,
+                    message: data.message || data.msg || "未知错误"
                 });
             }
+        } else {
+            $ui.alert({
+                title: "错误",
+                message: "空白数据"
+            });
         }
-    });
-}
+    }
+};
 
 function vipCheckin() {
     $http.post({
@@ -212,7 +170,7 @@ function liveCheckIn() {
     });
 }
 module.exports = {
-    mangaClockin,
+    mangaClockin: mangaCheckin,
     mangaCheckin,
     silverToCoin,
     vipCheckin,
