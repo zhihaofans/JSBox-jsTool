0e483dfdbb78bcb37152147ff2d3f35ccb0cce5f
zhihaofans <zhihaofans@hotmail.com>
2020-10-14 23:29:36 +0800

    Update mod/bilibili

--- scripts/mod/bilibili/common.js
+++ scripts/mod/bilibili/common.js
@@ -1,11 +1,25 @@
 module.exports = {
-    Comic
+    Comic,
+    Live,
+    User
 };
 let $B_useragent = require("./user_agent");
 let $B_api = require("./api");
 class Comic {
     constructor() {
         this._UA = new $B_useragent.Comic();
-
+        this._API = new $B_api.Comic();
+    }
+}
+class User {
+    constructor() {
+        this._UA = new $B_useragent.User();
+        this._API = new $B_api.User();
+    }
+}
+class Live {
+    constructor() {
+        this._UA = new $B_useragent.Live();
+        this._API = new $B_api.Live();
     }
 }
