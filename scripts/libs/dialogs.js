aa6df7c0e3fa78657b710410cf61ebbcf3326523
zhihaofans <zhihaofans@hotmail.com>
2020-12-22 19:04:23 +0800

    Add libs/dialogs.js

---
+++ scripts/libs/dialogs.js
@@ -0,0 +1,9 @@
+module.exports = {
+    plainAlert: require("./JSBoxDialogs/dialogs/plainAlert"),
+    inputAlert: require("./JSBoxDialogs/dialogs/inputAlert"),
+    loginAlert: require("./JSBoxDialogs/dialogs/loginAlert"),
+    textDialogs: require("./JSBoxDialogs/dialogs/textDialogs"),
+    listDialogs: require("./JSBoxDialogs/dialogs/listDialogs"),
+    editListDialogs: require("./JSBoxDialogs/dialogs/editListDialogs"),
+    formDialogs: require("./JSBoxDialogs/dialogs/formDialogs")
+};
