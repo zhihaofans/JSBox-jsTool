0c8bf4807cf6153d9412898d372e49fd27fcfff5
zhihaofans <zhihaofans@hotmail.com>
2020-12-23 14:02:06 +0800

    Update libs/dialogs.js

--- scripts/libs/dialogs.js
+++ scripts/libs/dialogs.js
@@ -1,10 +1,52 @@
 //JSBoxDialogs: https://github.com/Gandum2077/jsbox-dialogs
+
+let plainAlert = require("./JSBoxDialogs/dialogs/plainAlert"),
+    inputAlert = require("./JSBoxDialogs/dialogs/inputAlert"),
+    loginAlert = require("./JSBoxDialogs/dialogs/loginAlert"),
+    textDialogs = require("./JSBoxDialogs/dialogs/textDialogs"),
+    listDialogs = require("./JSBoxDialogs/dialogs/listDialogs"),
+    editListDialogs = require("./JSBoxDialogs/dialogs/editListDialogs"),
+    formDialogs = require("./JSBoxDialogs/dialogs/formDialogs"),
+    Dialogs = {
+        showPlainAlert: async (title, message) => {
+            return await plainAlert({
+                title: title,
+                message: message
+            });
+        },
+        showInputAlert: async (title) => {
+            return await inputAlert({
+                title: title
+            });
+        },
+        showLoginAlert: async (title) => {
+            return await loginAlert({
+                title: title
+            });
+        },
+        showTextDialogs: async (title, message) => {
+            return await textDialogs({
+                title: title,
+                message: message
+            });
+        },
+        showListDialogs: async (title, items, values, multiSelectEnabled = true, editable = true) => {
+            return await listDialogs({
+                title: title,
+                items: items,
+                values: values,
+                multiSelectEnabled: multiSelectEnabled,
+                editable: editable
+            });
+        }
+    };
 module.exports = {
-    plainAlert: require("./JSBoxDialogs/dialogs/plainAlert"),
-    inputAlert: require("./JSBoxDialogs/dialogs/inputAlert"),
-    loginAlert: require("./JSBoxDialogs/dialogs/loginAlert"),
-    textDialogs: require("./JSBoxDialogs/dialogs/textDialogs"),
-    listDialogs: require("./JSBoxDialogs/dialogs/listDialogs"),
-    editListDialogs: require("./JSBoxDialogs/dialogs/editListDialogs"),
-    formDialogs: require("./JSBoxDialogs/dialogs/formDialogs")
+    plainAlert,
+    inputAlert,
+    loginAlert,
+    textDialogs,
+    listDialogs,
+    editListDialogs,
+    formDialogs,
+    Dialogs
 };
