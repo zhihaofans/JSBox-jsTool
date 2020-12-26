dea69857009d84de6b46ec882da41c6999a505dc
zhihaofans <zhihaofans@hotmail.com>
2020-12-26 11:27:10 +0800

    JSBoxDialogs: 移除DEBUG代码

--- scripts/libs/JSBoxDialogs/utils/l10n.js
+++ scripts/libs/JSBoxDialogs/utils/l10n.js
@@ -1,19 +1,17 @@
-let $$res = require("./l10n_res"),
-    NOW_LANGUAGE = $device.info.language;
+let $$res = require("./l10n_res");
 
 function l10n(str_id) {
     var result = "";
     if (str_id) {
         const str_res = $$res[str_id];
         if (str_res) {
-            result = str_res[NOW_LANGUAGE] || $l10n(str_id);
+            result = str_res[$device.info.language] || $l10n(str_id);
         } else {
             result = $l10n(str_id);
         }
     } else {
         result = str_id;
     }
-    $console.warn(result);
     return result;
 }
 module.exports = l10n;
