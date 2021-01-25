b9d64ebb4a9d5672dceb8ad9503b80d0da4eeeb2
zhihaofans <zhihaofans@hotmail.com>
2021-01-25 14:50:00 +0800

    Update apple_rank.js

--- scripts/mod/ModV3/mods/apple_rank.js
+++ scripts/mod/ModV3/mods/apple_rank.js
@@ -1,17 +1,63 @@
-let appleRank = require("./apple/apple_rank"),
-    regionList = appleRank.getRegionCode(),
+let _apple = {
+        IOS_APP_STORE_RANK: {
+            HOST: "https://itunes.apple.com/",
+            FREE_APP: "/rss/topfreeapplications/limit=100/genre=25129/json",
+            PAID_APP: "/rss/toppaidapplications/limit=100/genre=25129/json",
+            GROSSING_APP:
+                "/rss/topgrossingapplications/limit=100/genre=25129/json"
+        }
+    },
+    regionCodeList = [
+        {
+            title: "中国大陆",
+            id: "cn"
+        },
+        {
+            title: "美国",
+            id: "us"
+        }
+    ],
+    regionList = getRegionCode(),
     rankResultItem = (_title, _url, _icon = undefined) => {
         this.title = _title;
         this.url = _url;
         this.icon = _icon;
+    },
+    myGet = url => {
+        return $http.get({
+            url: url
+        });
     };
-
+function iosRank(mode, region = "cn") {
+    const IOS_RANK = _apple.IOS_APP_STORE_RANK;
+    switch (mode) {
+        case "FREE_APP":
+            return myGet(IOS_RANK.HOST + region + IOS_RANK.FREE_APP);
+        case "PAID_APP":
+            return myGet(IOS_RANK.HOST + region + IOS_RANK.PAID_APP);
+        case "GROSSING_APP":
+            return myGet(IOS_RANK.HOST + region + IOS_RANK.GROSSING_APP);
+        default:
+            $ui.alert({
+                title: "iosRank error",
+                message: "mode error"
+            });
+            return undefined;
+    }
+}
+function RegionCode(_title, _id) {
+    this.title = _title;
+    this.id = _id;
+}
+function getRegionCode() {
+    return regionCodeList.map(i => new RegionCode(i.title, i.id));
+}
 function showResultList(mode) {
     $ui.loading(true);
     $ui.menu({
         items: regionList.map(i => i.title),
         handler: (title, idx) => {
-            appleRank.iosRank(mode, regionList[idx].id).then(function (resp) {
+            iosRank(mode, regionList[idx].id).then(function (resp) {
                 var data = resp.data;
                 if (data) {
                     const resultData = data.feed;
