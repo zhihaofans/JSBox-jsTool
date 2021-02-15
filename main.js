38e19a084005c2ab0228112f8fdec6df4ec30da2
zhihaofans <zhihaofans@hotmail.com>
2021-02-15 13:26:53 +0800

    Update main.js

--- main.js
+++ main.js
@@ -1,5 +1,22 @@
 let main = require("./scripts/main"),
+    loadMod = modId => {},
     init = () => {
-        main.loadMainView();
+        const query = $context.query;
+        $console.info(query);
+        if (query) {
+            switch (query.type) {
+                case "mod":
+                    if (query.mod_id) {
+                        loadMod(query.mod_id);
+                    } else {
+                        $ui.error("空白mod id");
+                    }
+                    break;
+                default:
+                    main.loadMainView();
+            }
+        } else {
+            main.loadMainView();
+        }
     };
 init();
