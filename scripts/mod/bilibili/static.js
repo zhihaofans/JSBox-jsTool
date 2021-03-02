de2c187144b18325804a1004f2ecc2c97ad8935b
zhihaofans <zhihaofans@hotmail.com>
2021-03-02 09:36:52 +0800

    Update mod - bilibili

--- scripts/mod/bilibili/static.js
+++ scripts/mod/bilibili/static.js
@@ -2,7 +2,7 @@ const UA = {
         COMIC: {
             CHECK_IN:
                 "comic-universal/802 CFNetwork/1125.2 Darwin/19.4.0 os/ios model/iPhone 11 mobi_app/iphone_comic osVer/13.4 network/2",
-            COUPON:
+            TICKET_STATES:
                 "https://manga.bilibili.com/twirp/user.v1.User/GetStates?access_key="
         },
         USER: {
