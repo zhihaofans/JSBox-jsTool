e4dd65d0e6c2118ee3f6977ac72a66e33445fe29
zhihaofans <zhihaofans@hotmail.com>
2021-03-06 20:49:00 +0800

    移除废弃代码

--- scripts/api/urlData.js
+++ scripts/api/urlData.js
@@ -1,10 +1,14 @@
 let ACFUN = {
         LOGIN: "https://id.app.acfun.cn/rest/app/login/signin",
-        GET_USER_INFO: "https://api-new.app.acfun.cn/rest/app/user/personalInfo",
-        DOWNLOAD_VIDEO: "https://api-new.app.acfun.cn/rest/app/play/playInfo/mp4",
-        GET_VIDEO_INFO: "https://api-new.app.acfun.cn/rest/app/douga/info?dougaId=",
+        GET_USER_INFO:
+            "https://api-new.app.acfun.cn/rest/app/user/personalInfo",
+        DOWNLOAD_VIDEO:
+            "https://api-new.app.acfun.cn/rest/app/play/playInfo/mp4",
+        GET_VIDEO_INFO:
+            "https://api-new.app.acfun.cn/rest/app/douga/info?dougaId=",
         SIGN_IN: "https://api-new.app.acfun.cn/rest/app/user/signIn",
-        GET_UPLOADER_VIDEO: "https://api-new.app.acfun.cn/rest/app/user/resource/query",
+        GET_UPLOADER_VIDEO:
+            "https://api-new.app.acfun.cn/rest/app/user/resource/query",
         ACFUN_DETAIL_VIDEO: "acfun://detail/video/",
         ACFUN_WWW_V_AC: "https://www.acfun.cn/v/ac",
         ACFUN_M_V_AC: "https://m.acfun.cn/v/?",
@@ -17,21 +21,21 @@ let ACFUN = {
         ACFUN_DETAIL_UPLOADER: "acfun://detail/uploader/"
     },
     API_66MZ8_COM = {
-        PHONE_WALLPAPER: "https://api.66mz8.com/api/rand.img.php?type=%E5%A3%81%E7%BA%B8"
+        PHONE_WALLPAPER:
+            "https://api.66mz8.com/api/rand.img.php?type=%E5%A3%81%E7%BA%B8"
     },
     JISHUWEN = {
         MAIN: "https://www.jishuwen.com/?p="
     },
     JSBOX = {
-        APP_CONFIG: "https://cdn.jsdelivr.net/gh/zhihaofans/JSBox-jsTool@master/config.json",
-        APP_CONFIG_GITHUB: "https://github.com/zhihaofans/JSBox-jsTool/raw/master/config.json",
-        APP_ICON: "https://cdn.jsdelivr.net/gh/zhihaofans/JSBox-jsTool@master/assets/icon.png",
-        APP_ICON_GITHUB: "https://github.com/zhihaofans/JSBox-jsTool/raw/master/assets/icon.png"
-    },
-    MEOWV = {
-        WALLPAPER: "https://api.meowv.com/wallpaper",
-        SOUL: "https://api.meowv.com/soul",
-        CAT: "https://api.meowv.com/common/cat"
+        APP_CONFIG:
+            "https://cdn.jsdelivr.net/gh/zhihaofans/JSBox-jsTool@master/config.json",
+        APP_CONFIG_GITHUB:
+            "https://github.com/zhihaofans/JSBox-jsTool/raw/master/config.json",
+        APP_ICON:
+            "https://cdn.jsdelivr.net/gh/zhihaofans/JSBox-jsTool@master/assets/icon.png",
+        APP_ICON_GITHUB:
+            "https://github.com/zhihaofans/JSBox-jsTool/raw/master/assets/icon.png"
     },
     TENCENT = {
         ADD_FRIENDS: "https://wpa.qq.com/msgrd?v=3&site=acfun.cn&menu=yes&uin="
@@ -44,7 +48,6 @@ module.exports = {
     API_66MZ8_COM,
     JISHUWEN,
     JSBOX,
-    MEOWV,
     TENCENT,
     TOPHUB
 };
--- scripts/mod/misc.js
+++ scripts/mod/misc.js
@@ -1,8 +1,5 @@
 let _url = require("../api/urlData.js");
 let mainList = [
-    "meowv/wallpaper",
-    "meowv/cat",
-    "meowv/soul",
     "66mz8/phoneWallpaper",
     "neeemooo"
 ];
@@ -30,16 +27,7 @@ let initListView = () => {
                         // const section = indexPath.section;
                         const row = indexPath.row;
                         switch (_data) {
-                            case "meowv/wallpaper":
-                                meomvwallpaper();
-                                break;
-                            case "meowv/cat":
-                                meowvRandomCat();
-                                break;
-                            case "meowv/soul":
-                                meomvSoul();
-                                break;
-                            case "66mz8/phoneWallpaper":
+                        case "66mz8/phoneWallpaper":
                                 api66mz8_PhoneWallpaper();
                                 break;
                             case "neeemooo":
@@ -52,158 +40,6 @@ let initListView = () => {
                 }
             }
         ]
-    });
-};
-let meomvwallpaper = () => {
-    $ui.loading(true);
-    $http
-        .get({
-            url: _url.MEOWV.WALLPAPER
-        })
-        .then(function (resp) {
-            var data = resp.data;
-            if (data) {
-                if (data.result) {
-                    let wpResult = data.result;
-                    $ui.menu({
-                        items: ["标题列表(快)", "直接预览(慢)"],
-                        handler: function (title, idx) {
-                            switch (idx) {
-                                case 0:
-                                    $ui.push({
-                                        props: {
-                                            title: "meowv/wallpaper"
-                                        },
-                                        views: [
-                                            {
-                                                type: "list",
-                                                props: {
-                                                    data: wpResult.map(
-                                                        wp => wp.title
-                                                    ),
-                                                    menu: {
-                                                        title: "菜单",
-                                                        items: [
-                                                            {
-                                                                title:
-                                                                    "Preview",
-                                                                symbol: "photo",
-                                                                handler: (
-                                                                    sender,
-                                                                    menuIdx
-                                                                ) => {
-                                                                    let thisWallpaper =
-                                                                        wpResult[
-                                                                            menuIdx
-                                                                                .row
-                                                                        ];
-                                                                    $ui.preview(
-                                                                        {
-                                                                            title:
-                                                                                thisWallpaper.title,
-                                                                            url:
-                                                                                thisWallpaper.url
-                                                                        }
-                                                                    );
-                                                                }
-                                                            },
-                                                            {
-                                                                title: "分享",
-                                                                symbol: "share",
-                                                                handler: (
-                                                                    sender,
-                                                                    menuIdx
-                                                                ) => {
-                                                                    $share.sheet(
-                                                                        [
-                                                                            wpResult[
-                                                                                menuIdx
-                                                                                    .row
-                                                                            ]
-                                                                                .url
-                                                                        ]
-                                                                    );
-                                                                }
-                                                            }
-                                                        ]
-                                                    }
-                                                },
-                                                layout: $layout.fill,
-                                                events: {
-                                                    didSelect: function (
-                                                        _sender,
-                                                        indexPath,
-                                                        _data
-                                                    ) {
-                                                        const row =
-                                                            indexPath.row;
-                                                        quicklookImageUrl(
-                                                            wpResult[row].url
-                                                        );
-                                                    }
-                                                }
-                                            }
-                                        ]
-                                    });
-                                    break;
-                                case 1:
-                                    $quicklook.open({
-                                        list: wpResult.map(wp => wp.url)
-                                    });
-                                    break;
-                            }
-                        }
-                    });
-                } else {
-                    $ui.alert({
-                        title: "错误",
-                        message: "空白结果"
-                    });
-                }
-            } else {
-                $ui.alert({
-                    title: "错误",
-                    message: "空白数据"
-                });
-            }
-        });
-};
-let meowvRandomCat = () => {
-    $ui.preview(_url.MEOWV.CAT);
-};
-let meomvSoul = () => {
-    $ui.loading(true);
-    $http.get({
-        url: _url.MEOWV.SOUL,
-        handler: resp => {
-            var data = resp.data;
-            $ui.loading(false);
-            if (data.success) {
-                $ui.alert({
-                    title: "鸡汤",
-                    message: data.result,
-                    actions: [
-                        {
-                            title: "复制",
-                            disabled: false, // Optional
-                            handler: function () {
-                                $clipboard.copy({
-                                    text: data.result,
-                                    ttl: 60
-                                });
-                            }
-                        },
-                        {
-                            title: "关闭",
-                            disabled: false, // Optional
-                            handler: function () {}
-                        }
-                    ]
-                });
-            } else {
-                $ui.error("Error");
-            }
-        }
     });
 };
 let api66mz8_PhoneWallpaper = () => {
