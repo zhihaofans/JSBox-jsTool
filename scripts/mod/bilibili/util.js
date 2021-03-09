03105f5e99442b081532a1f68273df9129b7c875
zhihaofans <zhihaofans@hotmail.com>
2021-03-09 18:48:22 +0800

    Update

--- scripts/mod/bilibili/util.js
+++ scripts/mod/bilibili/util.js
@@ -18,6 +18,19 @@ const table = "fZodR9XQDSUm21yCkr6zBqiveYah8bt4xsWpHnJE7jL5VG3guMTKNPAwcF",
                 r[s[i]] = table[Math.floor(av / 58 ** i) % 58];
             }
             return r.join("");
+        },
+        getOnline: async (type, id) => {
+            // bvid: resp.data.data.bvid,
+            // aid: resp.data.data.aid
+            return $http.get({
+                url: `${_BILIBILI.AV_BV_ONLINE}?${type}id=${id}`
+            });
+        },
+        getAvOnline: bv => {
+            return AVBV.getOnline("a", bv);
+        },
+        getBvOnline: av => {
+            return AVBV.getOnline("bv", av);
         }
     };
 for (let i = 0; i < 58; i++) {
