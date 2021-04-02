7d1b1311726c7d9d1611167923fe6a0367f2e834
zhihaofans <zhihaofans@hotmail.com>
2021-04-02 19:07:23 +0800

    mod/bilibili: 完善了关注的直播

--- scripts/mod/bilibili/live.js
+++ scripts/mod/bilibili/live.js
@@ -1,5 +1,7 @@
 const $B_user = require("./user"),
   $_Static = require("./static"),
+  $$ = require("$$"),
+  BiliScheme = require("AppScheme").Video.Bilibili,
   User = {
     autoCheckIn: async () => {
       const header = {
@@ -208,7 +210,6 @@ const $B_user = require("./user"),
     }
   },
   View = {
-    //TODO
     myFollow: () => {
       $ui.loading(true);
       $ui.menu({
@@ -217,7 +218,7 @@ const $B_user = require("./user"),
           switch (idx) {
             case 0:
               try {
-                View.showMyFollower(true);
+                View.showOnlineFollower();
               } catch (error) {
                 $ui.loading(false);
                 $console.error(error);
@@ -229,8 +230,7 @@ const $B_user = require("./user"),
               break;
             case 1:
               try {
-                View.showMyFollower(false);
-                $ui.loading(false);
+                View.showOfflineFollower();
               } catch (error) {
                 $ui.loading(false);
                 $console.error(error);
@@ -246,78 +246,179 @@ const $B_user = require("./user"),
         }
       });
     },
-    showMyFollower: async (online = true) => {
-      const httpData = await Liver.getFollower(online);
+    showOnlineFollower: async () => {
+      const httpData = await Liver.onlineFollower();
       if (httpData) {
         if (httpData.code === 0) {
           const myFollowData = httpData.data,
-            myFollowList = myFollowData.data.rooms;
-          $console.warn(myFollowData);
-          if (
-            myFollowData.data.total_count === 0 ||
-            myFollowList.length === 0
-          ) {
-            $ui.alert({
-              title: "关注列表为空，请登陆或关注主播",
-              message: `showMyFollower.count = 0(online=${online})`
-            });
-            $ui.loading(false);
-          } else {
-            $ui.loading(false);
-            $ui.push({
-              props: {
-                title: ""
-              },
-              views: [
-                {
-                  type: "list",
-                  props: {
-                    data: myFollowList.map(liveRoom => {
-                      return {
-                        title: `@${liveRoom.uname}`,
-                        rows: [
-                          online
-                            ? liveRoom.title
-                            : liveRoom.announcement_content,
-                          `直播间id:${liveRoom.roomid}`,
-                          online
-                            ? `直播间封面:${liveRoom.cover.repleace(
-                                "http://i0.hdslb.com/bfs/live/new_room_cover/",
-                                ""
-                              )}`
-                            : `${liveRoom.live_desc}播了`
-                        ]
-                      };
-                    })
-                  },
-                  layout: $layout.fill,
-                  events: {
-                    didSelect: function (_sender, indexPath, _data) {
-                      const section = indexPath.section,
-                        row = indexPath.row,
-                        thisRoom = myFollowList[section];
-                      switch (row) {
-                        case 0:
-                          $app.openURL(thisRoom.link);
-                          break;
-                        case 1:
-                          $app.openURL(`bilibili://live/${thisRoom.roomid}`);
-                          break;
-                        case 2:
-                          if (online) {
-                            $quicklook.open({
-                              url: thisRoom.cover,
-                              handler: function () {
-                                $console.info(thisRoom.cover);
-                              }
-                            });
-                          }
-                          break;
+            myFollowList = myFollowData.rooms;
+          if (myFollowList) {
+            if (myFollowData.total_count === 0 || myFollowList.length === 0) {
+              $ui.alert({
+                title: "在播关注列表为空，请登陆、关注更多主播或者催主播开播",
+                message: `showOnlineFollower.count = 0`
+              });
+              $ui.loading(false);
+            } else {
+              $ui.loading(false);
+              $ui.push({
+                props: {
+                  title: `${myFollowData.total_count}人在播`
+                },
+                views: [
+                  {
+                    type: "list",
+                    props: {
+                      data: myFollowList.map(liveRoom => {
+                        const official_verify_list = {
+                          "-1": "未认证",
+                          0: "个人认证",
+                          1: "企业认证"
+                        };
+                        return {
+                          title: `@${liveRoom.uname}（${liveRoom.live_tag_name}）`,
+                          rows: [
+                            liveRoom.title,
+                            `直播间id：${liveRoom.roomid}`,
+                            `uid：${liveRoom.uid}`,
+                            `直播间封面`,
+                            `认证：${
+                              official_verify_list[
+                                new String(liveRoom.official_verify)
+                              ] || "[未知]"
+                            }`
+                          ]
+                        };
+                      })
+                    },
+                    layout: $layout.fill,
+                    events: {
+                      didSelect: function (_sender, indexPath, _data) {
+                        const section = indexPath.section,
+                          row = indexPath.row,
+                          thisRoom = myFollowList[section];
+                        switch (row) {
+                          case 0:
+                            $app.openURL(thisRoom.link);
+                            break;
+                          case 1:
+                            BiliScheme.live(thisRoom.roomid);
+                            break;
+                          case 2:
+                            BiliScheme.space(thisRoom.uid);
+                            break;
+                          case 3:
+                            $$.Image.single.showImageMenu(thisRoom.cover);
+                            break;
+                        }
                       }
                     }
                   }
-                }
-              ]
+                ]
+              });
+            }
+          } else {
+            $ui.loading(false);
+            $console.warn(myFollowData);
+            $ui.alert({
+              title: "未知错误",
+              message: "返回空白数据，请检查网络是否正常"
+            });
+          }
+        } else {
+          $ui.loading(false);
+          $console.error(httpData);
+          $ui.alert({
+            title: `错误代码${httpData.code}`,
+            message: httpData.message
+          });
+        }
+      } else {
+        $ui.loading(false);
+        $console.error("返回空白数据，请检查网络是否正常");
+        $ui.alert({
+          title: "未知错误",
+          message: "返回空白数据，请检查网络是否正常"
+        });
+      }
+    },
+    showOfflineFollower: async () => {
+      const httpData = await Liver.offlineFollower();
+      if (httpData) {
+        if (httpData.code === 0) {
+          const myFollowData = httpData.data,
+            myFollowList = myFollowData.rooms;
+          if (myFollowList) {
+            if (myFollowData.total_count === 0 || myFollowList.length === 0) {
+              $ui.alert({
+                title: "未播关注列表为空，请登陆、关注更多主播或者催主播下播",
+                message: `showOfflineFollower.count = 0`
+              });
+              $ui.loading(false);
+            } else {
+              $ui.loading(false);
+              $ui.push({
+                props: {
+                  title: `${myFollowData.total_count}人未播`
+                },
+                views: [
+                  {
+                    type: "list",
+                    props: {
+                      data: myFollowList.map(liveRoom => {
+                        const official_verify_list = {
+                          "-1": "未认证",
+                          0: "个人认证",
+                          1: "企业认证"
+                        };
+                        return {
+                          title: `@${liveRoom.uname}`,
+                          rows: [
+                            `${liveRoom.live_desc} 播了`,
+                            `直播间id：${liveRoom.roomid}`,
+                            `uid：${liveRoom.uid}`,
+                            `个人头像`,
+                            `认证：${
+                              official_verify_list[
+                                new String(liveRoom.official_verify)
+                              ] || "[未知]"
+                            }`
+                          ]
+                        };
+                      })
+                    },
+                    layout: $layout.fill,
+                    events: {
+                      didSelect: function (_sender, indexPath, _data) {
+                        const section = indexPath.section,
+                          row = indexPath.row,
+                          thisRoom = myFollowList[section];
+                        switch (row) {
+                          case 0:
+                            $app.openURL(thisRoom.link);
+                            break;
+                          case 1:
+                            BiliScheme.live(thisRoom.roomid);
+                            break;
+                          case 2:
+                            BiliScheme.space(thisRoom.uid);
+                            break;
+                          case 3:
+                            $$.Image.single.showImageMenu(thisRoom.face);
+                            break;
+                        }
+                      }
+                    }
+                  }
+                ]
+              });
+            }
+          } else {
+            $ui.loading(false);
+            $console.warn(myFollowData);
+            $ui.alert({
+              title: "未知错误",
+              message: "返回空白数据，请检查网络是否正常"
             });
           }
         } else {
