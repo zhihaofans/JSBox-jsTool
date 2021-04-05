935bf5f2c6c9f1b1fc2bfb9877dd64c0a27e35bb
zhihaofans <zhihaofans@hotmail.com>
2021-04-05 19:06:35 +0800

    feat(mod/downloader): add routers

---
+++ scripts/mod/downloader.js
@@ -0,0 +1,2 @@
+const init = () => {};
+module.exports = { init };
--- scripts/mod/downloader/router.js
+++ scripts/mod/downloader/router.js
@@ -1,2 +1,14 @@
-module.exports={
+module.exports = {
+  "pixiv.cat.artworks.single": {
+    script: "pixiv",
+    app: "pixivCat",
+    func: "single",
+    regex: /https:\/\/www.pixiv.net\/artworks\/(\d+)/
+  },
+  "pixiv.cat.artworks.mult": {
+    script: "pixiv",
+    app: "pixivCat",
+    func: "mult",
+    regex: /https:\/\/www.pixiv.net\/artworks\/(\d+)/
+  }
 };
