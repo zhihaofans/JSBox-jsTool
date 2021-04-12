fe0b038c78fd2f2c5d315ca45e573419b5bed54b
zhihaofans <zhihaofans@hotmail.com>
2021-04-12 15:13:51 +0800

    test(main.js): remove mod urlscheme loader

--- main.js
+++ main.js
@@ -1,21 +1,18 @@
 let main = require("./scripts/main"),
-  mod = require("./scripts/mod_index"),
   l10nInit = require("./strings/l10n_init"),
-  loadMod = modId => {
-    mod.initMod(modId);
-  },
   init = () => {
     $app.strings = l10nInit.init();
     const query = $context.query;
     if (query) {
       switch (query.type) {
-        case "mod":
+        // TODO:load mod by url scheme
+        /* case "mod":
           if (query.mod_id) {
             loadMod(query.mod_id);
           } else {
             $ui.error("空白mod id");
           }
-          break;
+          break; */
         default:
           main.loadMainView();
       }
