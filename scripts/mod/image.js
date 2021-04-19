d1441db7cdd818b296f4833b7fa201cb2d797cfc
zhihaofans <zhihaofans@hotmail.com>
2021-04-19 13:23:17 +0800

    pref(mod/image): let=>const

--- scripts/mod/image.js
+++ scripts/mod/image.js
@@ -1,58 +1,59 @@
-let app = require("/scripts/api/app.js"),
-  $$ = require("$$");
-
-let docScan = () => {
-  $photo.scan({
-    handler: data => {
-      $console.info(data);
-      if (data.status) {
-        const resultList = data.results;
-        if (resultList.length > 0) {
-          let imageDataList = [];
-          for (i in resultList) {
-            imageDataList.push(resultList[i].png);
-          }
-          $ui.push({
-            props: {
-              title: $l10n("SCAN_SUCCESS")
-            },
-            views: [
-              {
-                type: "list",
-                props: {
-                  data: $$.Str.getListFromL10n(["预览全部图片", "保存全部图片"])
-                },
-                layout: $layout.fill,
-                events: {
-                  didSelect: function (_sender, indexPath, _data) {
-                    switch (indexPath.row) {
-                      case 0:
-                        $quicklook.open({
-                          list: imageDataList
-                        });
-                        break;
-                      default:
+const $$ = require("$$"),
+  docScan = () => {
+    $photo.scan({
+      handler: data => {
+        $console.info(data);
+        if (data.status) {
+          const resultList = data.results;
+          if (resultList.length > 0) {
+            let imageDataList = [];
+            for (const i in resultList) {
+              imageDataList.push(resultList[i].png);
+            }
+            $ui.push({
+              props: {
+                title: $l10n("SCAN_SUCCESS")
+              },
+              views: [
+                {
+                  type: "list",
+                  props: {
+                    data: $$.Str.getListFromL10n([
+                      "预览全部图片",
+                      "保存全部图片"
+                    ])
+                  },
+                  layout: $layout.fill,
+                  events: {
+                    didSelect: function (_sender, indexPath, _data) {
+                      switch (indexPath.row) {
+                        case 0:
+                          $quicklook.open({
+                            list: imageDataList
+                          });
+                          break;
+                        default:
+                      }
                     }
                   }
                 }
-              }
-            ]
-          });
+              ]
+            });
+          } else {
+            $ui.alert({
+              title: $l10n("SCAN_FAILED"),
+              message: "系统返回0个结果"
+            });
+          }
         } else {
           $ui.alert({
             title: $l10n("SCAN_FAILED"),
-            message: "系统返回0个结果"
+            message: data.error.description
           });
         }
-      } else {
-        $ui.alert({
-          title: $l10n("SCAN_FAILED"),
-          message: data.error.description
-        });
       }
-    }
-  });
-};
+    });
+  };
 
 let init = () => {
   $ui.push({
