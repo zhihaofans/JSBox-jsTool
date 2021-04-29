f8e0d2df2053785f514a0b060bfc7f6f1f48f6ab
zhihaofans <zhihaofans@hotmail.com>
2021-04-29 19:26:01 +0800

    test(mod/config): update

--- scripts/mod/config/mod.js
+++ scripts/mod/config/mod.js
@@ -2,6 +2,7 @@ const Lib = require("./lib");
 class Mod {
   constructor(mod) {
     this.MOD_ID = mod;
+    this.MOD_DATABASE_ID = "MOD_DATABASE";
   }
   getConfig(key) {
     const Cache = new Lib.Cache(`mod.${this.MOD_ID}.${key}`);
@@ -11,5 +12,10 @@ class Mod {
     const Cache = new Lib.Cache(`mod.${this.MOD_ID}.${key}`);
     return Cache.set(value);
   }
+  getSQL() {
+    const SQLite = new Lib.SQLite(this.MOD_DATABASE_ID);
+    //`mod.${this.MOD_ID}.${key}`
+  }
 }
-module.exports = { Mod };
+
+module.exports = Mod;
