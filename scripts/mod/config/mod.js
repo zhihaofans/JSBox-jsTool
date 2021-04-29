f4c57bd46feddc9fbc73d48647cc24c17b53b161
zhihaofans <zhihaofans@hotmail.com>
2021-04-29 19:21:09 +0800

    test(mod/config): update

--- scripts/mod/config/mod.js
+++ scripts/mod/config/mod.js
@@ -1,3 +1,15 @@
-const getConfig = (mod, key) => {
-  const configId = `mod.${mod}.${key}`;
-};
+const Lib = require("./lib");
+class Mod {
+  constructor(mod) {
+    this.MOD_ID = mod;
+  }
+  getConfig(key) {
+    const Cache = new Lib.Cache(`mod.${this.MOD_ID}.${key}`);
+    return Cache.get();
+  }
+  setConfig(key, value) {
+    const Cache = new Lib.Cache(`mod.${this.MOD_ID}.${key}`);
+    return Cache.set(value);
+  }
+}
+module.exports = { Mod };
