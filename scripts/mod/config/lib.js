b2d2408029bcbc046b8f795310871788347faf32
zhihaofans <zhihaofans@hotmail.com>
2021-05-02 17:49:19 +0800

    test(mod/config): fix bugs

--- scripts/mod/config/lib.js
+++ scripts/mod/config/lib.js
@@ -8,23 +8,18 @@ class SQLite {
   update(sql, args = undefined) {
     this.createSimpleTable();
     const db = this.init();
-    db.update(
-      args
-        ? sql
-        : {
-            sql: sql,
-            args: args
-          }
-    );
+    db.update({
+      sql: sql,
+      args: args
+    });
     db.close();
   }
   createSimpleTable(table_id) {
     if (table_id) {
       try {
         const db = this.init(),
-          sql = `CREATE TABLE IF NOT EXISTS ?(id TEXT PRIMARY KEY NOT NULL, value TEXT)`,
-          args = [table_id];
-        db.update(sql, args);
+          sql = `CREATE TABLE IF NOT EXISTS ${table_id}(id TEXT PRIMARY KEY NOT NULL, value TEXT)`;
+        db.update({ sql: sql, args: undefined });
         db.close();
       } catch (_ERROR) {
         $console.error(_ERROR);
@@ -34,66 +29,90 @@ class SQLite {
     }
   }
   parseSimpleQuery(result) {
-    if (result) {
-      if (result.error !== null) {
-        $console.error(result.error);
+    try {
+      if (result) {
+        if (result.error !== null) {
+          $console.error(result.error);
+          return undefined;
+        }
+        const sqlResult = result.result,
+          data = [];
+        while (sqlResult.next()) {
+          data.push({
+            id: sqlResult.get("id"),
+            value: sqlResult.get("value")
+          });
+        }
+        sqlResult.close();
+        return data;
+      } else {
         return undefined;
       }
-      const sqlResult = result.result,
-        data = [];
-      while (sqlResult.next()) {
-        data.push({
-          id: sqlResult.get("id"),
-          value: sqlResult.get("value")
-        });
-      }
-      sqlResult.close();
-      return data;
-    } else {
+    } catch (_ERROR) {
+      $console.error(`Config.lib.parseSimpleQuery:${_ERROR.message}`);
       return undefined;
     }
   }
   getSimpleData(table, key) {
-    this.createSimpleTable(table);
-    const db = this.init(),
-      sql = "SELECT * FROM ? WHERE id = ?",
-      args = [table, key],
-      result = db.query({
-        sql: sql,
-        args: args
-      }),
-      sql_data = this.parseSimpleQuery(result);
-    return sql_data.length === 1 ? sql_data[0].value : undefined;
+    try {
+      if (table && key) {
+        this.createSimpleTable(table);
+        const db = this.init(),
+          sql = `SELECT * FROM ${table} WHERE id = ?`,
+          args = [key],
+          result = db.query({
+            sql: sql,
+            args: args
+          }),
+          sql_data = this.parseSimpleQuery(result);
+        if (sql_data && sql_data.length === 1) {
+          return sql_data[0].value;
+        } else {
+          return undefined;
+        }
+      } else {
+        return undefined;
+      }
+    } catch (_ERROR) {
+      $console.error(`Config.lib.getSimpleData:${_ERROR.message}`);
+      return undefined;
+    }
   }
   setSimpleData(table, key, value) {
-    this.createSimpleTable(table);
-    if (key) {
-      const db = this.init(),
-        sql = this.getSimpleData(table, key)
-          ? "UPDATE ? SET value=? WHERE id=?"
-          : "INSERT INTO ? (value,id) VALUES (?, ?)",
-        update_result = db.update({
-          sql: sql,
-          args: [table, value, key]
-        });
-      db.close();
-      return update_result.result || false;
-    } else {
+    try {
+      if (table && key) {
+        this.createSimpleTable(table);
+        const db = this.init(),
+          sql = this.getSimpleData(table, key)
+            ? `UPDATE ${table} SET value=? WHERE id=?`
+            : `INSERT INTO ${table} (value,id) VALUES (?, ?)`,
+          args = [value, key],
+          update_result = db.update({
+            sql: sql,
+            args: args
+          });
+        db.close();
+        return update_result.result || false;
+      } else {
+        return false;
+      }
+    } catch (_ERROR) {
+      $console.error(`Config.lib.setSimpleData:${_ERROR.message}`);
       return false;
     }
   }
   auto(table, sql_key, value = undefined) {
-    if (!sql_key) {
+    this.createSimpleTable(table);
+    if (!sql_key || !table) {
       return undefined;
     }
     try {
       if (value) {
-        $console.warn(`${sql_key}:${value.toString()}`);
         this.setSimpleData(table, sql_key, value.toString());
       }
       return this.getSimpleData(table, sql_key) || undefined;
     } catch (_ERROR) {
-      $console.error(_ERROR);
+      $console.error(`Config.lib.auto:${_ERROR.message}`);
       return undefined;
     }
   }
