86ddb0f1ac0034d0abc0e65569f63da999dff16e
zhihaofans <zhihaofans@hotmail.com>
2021-05-19 14:06:50 +0800

    test(mod/wallhaven): update core.js support

--- scripts/mod/wallhaven.js
+++ scripts/mod/wallhaven.js
@@ -54,10 +54,12 @@ class Wallhaven extends Core {
     }
   }
   async animeRandom() {
+    const SuperThis = this;
     $ui.loading(true);
-    const query = `id%3A5type:png`,
+    const next_seed_id = "nextseed",
+      query = `id%3A5type:png`,
       sorting = `random`,
-      randomSeed = this.getSql("nextseed") || `XekqJ6`,
+      randomSeed = this.getSql(next_seed_id) || `XekqJ6`,
       page = 1,
       purity = "111",
       categories = "010",
@@ -71,6 +73,74 @@ class Wallhaven extends Core {
         title: "misc.wallhaven.error",
         message: httpResult.error.message
       });
+    } else if (httpResult.data) {
+      const httpData = httpResult.data,
+        apiData = httpData.data,
+        apiMeta = httpData.meta,
+        nextRandomSeed = apiMeta.seed;
+      $console.info(`nextRandomSeed:${nextRandomSeed}`);
+      this.setSql(next_seed_id, nextRandomSeed || randomSeed);
+      $ui.loading(false);
+      if (apiData && apiMeta) {
+        $ui.push({
+          props: {
+            title: apiMeta.query.tag,
+            navButtons: [
+              {
+                title: "api_key",
+                symbol: "person", // SF symbols are supported
+                handler: sender => {
+                  const api_key = this.getSql("api_key");
+                  $input.text({
+                    type: $kbType.text,
+                    placeholder: "api_key",
+                    text: api_key,
+                    handler: function (input) {
+                      if (input) {
+                        SuperThis.setSql("api_key", input);
+                      }
+                    }
+                  });
+                }
+              }
+            ]
+          },
+          views: [
+            {
+              type: "list",
+              props: {
+                menu: {
+                  title: "Context Menu",
+                  items: [
+                    {
+                      title: "查看全部",
+                      handler: sender => {}
+                    }
+                  ]
+                },
+                data: apiData.map(
+                  item => `${item.category} | ${item.purity} | ${item.id}`
+                )
+              },
+              layout: $layout.fill,
+              events: {
+                didSelect: function (_sender, indexPath, _data) {
+                  const View = new SuperThis.$_.View();
+                  View.Image.showSingleMenu(apiData[indexPath.row].path);
+                }
+              }
+            }
+          ]
+        });
+      } else {
+        $ui.loading(false);
+        $console.error(`misc.wallhaven:(${httpData.code})${httpData.message}`);
+        return;
+      }
+    } else {
+      $ui.loading(false);
+      $console.error(`misc.wallhaven:(httpResult.data==undefined`);
+      return;
     }
   }
 }
