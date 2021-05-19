c20d5acf227942cfe82f8ea69c14d89c487b48af
zhihaofans <zhihaofans@hotmail.com>
2021-05-19 18:48:26 +0800

    test(Core.js): update use guide

--- scripts/mod/wallhaven.js
+++ scripts/mod/wallhaven.js
@@ -130,22 +130,24 @@ class Wallhaven extends Core {
     }
   }
 }
-const run = () => {
-  const _wallhaven = new Wallhaven();
-  const ver = _wallhaven.checkCoreVersion();
-  if (ver === 0) {
-    _wallhaven.initView();
-    return new Result({
-      success: true,
-      code: 0
-    });
-  } else {
-    return new Result({
-      success: false,
-      code: 1,
-      error_message: `need update core.js(${ver})`
-    });
+
+module.exports = {
+  _SUPPORT_COREJS_: 1,
+  run: () => {
+    const _wallhaven = new Wallhaven();
+    const ver = _wallhaven.checkCoreVersion();
+    if (ver === 0) {
+      _wallhaven.initView();
+      return new Result({
+        success: true,
+        code: 0
+      });
+    } else {
+      return new Result({
+        success: false,
+        code: 1,
+        error_message: `need update core.js(${ver})`
+      });
+    }
   }
 };
-
-module.exports = { run };
