aed3aabbfdb10a2f7df74f75f64afad440994f18
zhihaofans <zhihaofans@hotmail.com>
2021-05-21 12:44:29 +0800

    test(mod/bilibili): update core.js support

--- scripts/mod/bilibili.js
+++ scripts/mod/bilibili.js
@@ -1,3 +1,24 @@
+const coreFile = require("../core"),
+  Core = new coreFile.Core(),
+  Result = require("../core").Result,
+  MOD_NAME = "Bilibili",
+  MOD_VERSION = 1,
+  MOD_AUTHOR = "zhihaofans",
+  NEED_DATABASE = true,
+  DATABASE_ID = "bilibili",
+  NEED_CORE_VERSION = 1;
+class Bilibili extends Core {
+  constructor() {
+    super({
+      title: MOD_NAME,
+      version: MOD_VERSION,
+      author: MOD_AUTHOR,
+      need_database: NEED_DATABASE,
+      database_id: DATABASE_ID,
+      need_core_version: NEED_CORE_VERSION
+    });
+  }
+}
 module.exports = {
   init: require("./bilibili/init").init,
   _SUPPORT_COREJS_: 0,
